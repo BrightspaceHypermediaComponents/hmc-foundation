8354dacf1a865133410a48cb4606703ec4c60eac
ten.bradley@d2l.com <Ten.Bradley@D2L.com>
2021-01-05 15:52:25 -0500

    skipped tests based on investigation with Maya

--- test/activity/description/d2l-activity-description-editor.test.js
+++ test/activity/description/d2l-activity-description-editor.test.js
@@ -77,7 +77,7 @@ describe('d2l-activity-description-editor', () => {
 			assert.equal(element.description, learningPathNew.properties.description, 'description property should match');
 		});
 
-		describe('path:/learning-path/existing', () => {
+		describe.skip('path:/learning-path/existing', () => {
 			let element;
 			beforeEach(async() => {
 				clearStore();
