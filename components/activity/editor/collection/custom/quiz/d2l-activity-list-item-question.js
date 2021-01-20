4115f7f890e86f468de0e178fa1140a63bf0c0c0
Steven Ao <Steven.Ao@D2L.com>
2021-01-20 11:41:11 -0800

    fix lint

--- components/activity/editor/collection/custom/quiz/d2l-activity-list-item-question.js
+++ components/activity/editor/collection/custom/quiz/d2l-activity-list-item-question.js
@@ -5,8 +5,8 @@ import '@brightspace-ui/core/components/colors/colors';
 
 import { bodyCompactStyles, bodySmallStyles, bodyStandardStyles, heading3Styles, labelStyles  } from '@brightspace-ui/core/components/typography/styles.js';
 import { css, LitElement } from 'lit-element/lit-element.js';
-import { customHypermediaElement, html } from '@brightspace-hmc/foundation-engine/framework/lit/hypermedia-components.js';
 import { HypermediaStateMixin, observableTypes } from '@brightspace-hmc/foundation-engine/framework/lit/HypermediaStateMixin.js';
+import { html } from '@brightspace-hmc/foundation-engine/framework/lit/hypermedia-components.js';
 import { LocalizeQuizEditor } from './lang/localization.js';
 import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
 
