200329d2eb1501e50e2f7836478922f6bdd96d13
Alexander Kobets <alexander.kobets@D2L.com>
2021-01-29 10:09:19 -0500

    manually add translation for components/activity/editor/collection/lang

--- components/activity/editor/collection/lang/fr-fr.js
+++ components/activity/editor/collection/lang/fr-fr.js
@@ -7,10 +7,10 @@ export default {
 	"dialog-browseActivityLibrary": "Parcourir la bibliothèque d’activités", // The title of the dialog box that allows you to browse potential activities to add to the learning path.
 	"button-cancel": "Annuler", // When adding activities to the learning path, this will cancel the currently selected potential activities and close the dialog box.
 	"button-clearSelected": "Effacer la sélection", // When adding activities (bulk add) to the learning path this allows you to clear the ones currently selected.
-	"button-loadMore": "En voir plus", // When adding activities to the learning path, this is on a button that will load more potential activities that can be added that match the current search.
-	"list-noActivitiesFound": "Aucune activité n’a été trouvée au moyen de vos termes de recherche.", // Displayed when the learning path has no activities while in the screen that allows you to add them.
+	"button-loadMore": "Charger plus", // When adding activities to the learning path, this is on a button that will load more potential activities that can be added that match the current search.
+	"list-noActivitiesFound": "Aucune activité n’a été trouvée à l’aide de votre terme de recherche.", // Displayed when the learning path has no activities while in the screen that allows you to add them.
 	"label-search": "Rechercher", // When adding activities to the learning path, this is where you can search for potential activities to add.
-	"input-searchPlaceholder": "Recherche…", // Placeholder text for the search input to search the list of potential activities.
-	"dialog-selected": "{count} sélectionné(s).", // When adding activities (bulk add) to a learning path this is the number of activities that will be added to the list.
+	"input-searchPlaceholder": "Rechercher...", // Placeholder text for the search input to search the list of potential activities.
+	"dialog-selected": "{count} sélectionné(e)(s).", // When adding activities (bulk add) to a learning path this is the number of activities that will be added to the list.
 	"text-loadMoreError": "Failed to load activities", // When adding activities to the learning path, this is text will appear when the load more button results in an error.
 };
