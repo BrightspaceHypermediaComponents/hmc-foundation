caae1c088bdb67d0556743b2a7b2d06cd0c55341
ten.bradley@d2l.com <Ten.Bradley@D2L.com>
2021-02-24 14:57:38 -0500

    secondary card as a ui component. learning path card

--- components/activity/editor/collection/d2l-activity-editor-secondary-card.js
+++ components/activity/editor/collection/d2l-activity-editor-secondary-card.js
@@ -1,10 +1,18 @@
 
 import '@brightspace-ui-labs/accordion/accordion-collapse.js';
-import { css, LitElement } from 'lit-element/lit-element';
-import { customHypermediaElement, html } from '@brightspace-hmc/foundation-engine/framework/lit/hypermedia-components.js';
+import { css, html, LitElement } from 'lit-element/lit-element';
+import { customHypermediaElement } from '@brightspace-hmc/foundation-engine/framework/lit/hypermedia-components.js';
 import { labelStyles } from '@brightspace-ui/core/components/typography/styles.js';
 
 class ActivityEditorSecondaryCard extends LitElement {
+
+	static get properties() {
+		return {
+			titleText: { type: String, attribute: 'title-text' },
+			bodyText: { type: String, attribute: 'body-text' },
+			collapsable: { type: Boolean }
+		};
+	}
 
 	static get styles() {
 		return [labelStyles, css`
@@ -20,22 +28,51 @@ class ActivityEditorSecondaryCard extends LitElement {
 				padding: 20px;
 				padding-top: 0;
 			}
+			.d2l-activity-editor-card {
+				display: grid;
+				grid-template-rows: [start header] auto [content] auto [end];
+			}
+			::slotted([slot=header]) {
+				grid-row: header;
+			}
+			::slotted([slot=content]) {
+				grid-row: content;
+			}
 		`];
 	}
 
 	render() {
+		return this.collapsable ? this._renderCollapsable() : this._renderDefault();
+
+	}
+
+	_renderDefault() {
 		return html`
+			<span slot="header">
+			<h3>
+			${this.titleText}
+			</h3>
+			<hr>
+			</span>
+			<span class="content">
+			${this.bodyText}
+			</span>`;
+	}
+
+	_renderCollapsable() {
+		return html`<d2l-labs-accordion-collapse flex>
 		<span slot="header">
 		<h3>
-		Additional Identification
+		${this.titleText}
 		</h3>
 		<hr>
 		</span>
 		<span class="content">
-		Blah blah blah
+		${this.bodyText}
 		</span>
+		</d2l-labs-accordion-collapse>
 		`;
 	}
 }
 
-customHypermediaElement('d2l-activity-editor-card', ActivityEditorSecondaryCard);
+customHypermediaElement('d2l-activity-secondary-card', ActivityEditorSecondaryCard);
--- components/activity/editor/collection/d2l-activity-editor-sidebar-learning-path.js
+++ components/activity/editor/collection/d2l-activity-editor-sidebar-learning-path.js
@@ -1,9 +1,7 @@
-import '@brightspace-ui-labs/accordion/accordion-collapse.js';
-import '@brightspace-ui-labs/accordion/accordion.js';
-import './d2l-activity-editor-secondary-card.js';
-import { css, LitElement } from 'lit-element/lit-element.js';
-import { customHypermediaElement, html } from '@brightspace-hmc/foundation-engine/framework/lit/hypermedia-components.js';
+import './d2l-activity-card-learning-path.js';
+import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { HypermediaStateMixin, observableTypes } from '@brightspace-hmc/foundation-engine/framework/lit/HypermediaStateMixin.js';
+import { customHypermediaElement } from '@brightspace-hmc/foundation-engine/framework/lit/hypermedia-components.js';
 
 const rels = Object.freeze({
 	specialization: 'https://api.brightspace.com/rels/specialization'
@@ -13,6 +11,8 @@ class ActivityCollectionEditorSidebar extends HypermediaStateMixin(LitElement) {
 
 	static get properties() {
 		return {
+			href: { type: String, reflect: true },
+			token: { type: String },
 			_specializationHref: { type: String, observable: observableTypes.link, rel: rels.specialization }
 		};
 	}
@@ -33,10 +33,9 @@ class ActivityCollectionEditorSidebar extends HypermediaStateMixin(LitElement) {
 
 	render() {
 		return html`
-		<d2l-activity-editor-card>
-		</d2l-activity-editor-card>
+		<d2l-activity-card-learning-path href="${this.href}" .token="${this.token}" }></d2l-activity-card-learning-path>
 		`;
 	}
 }
 
-customHypermediaElement('d2l-activity-editor-sidebar-collection', ActivityCollectionEditorSidebar, 'd2l-activity-editor-sidebar', [['activity-collection'], ['learning-path']]);
+customHypermediaElement('d2l-activity-editor-sidebar-learning-path', ActivityCollectionEditorSidebar, 'd2l-activity-editor-sidebar', [['activity-collection'], ['learning-path']]);
