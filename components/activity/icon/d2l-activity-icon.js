6423a20e1886cfaf9d2ddad37c06e77562e34431
Zina Ramirez <instinctualart@gmail.com>
2021-03-17 14:44:40 -0400

    Updating the icon to include work to do widget effort

--- components/activity/icon/d2l-activity-icon.js
+++ components/activity/icon/d2l-activity-icon.js
@@ -1,19 +1,38 @@
 import '@brightspace-ui/core/components/icons/icon.js';
 import { html, LitElement } from 'lit-element/lit-element.js';
 import { HypermediaStateMixin, observableTypes } from '@brightspace-hmc/foundation-engine/framework/lit/HypermediaStateMixin.js';
+import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
 
-class ActivityIcon extends HypermediaStateMixin(LitElement) {
+/**
+ * Activity Icon foundation component that displays an icon when given an activity href.
+ * Looks for a subentity for configurable icon support. Defaults to standard mappings depending on the activity class
+ */
+class ActivityIcon extends SkeletonMixin(HypermediaStateMixin(LitElement)) {
 	static get properties() {
 		return {
-			classes: { type: Array, observable: observableTypes.classes }
+			classes: { observable: observableTypes.classes },
+			configuredIcon: { observable: observableTypes.subEntity, rel: 'icon',
+				method: (icon) => icon.class.includes('tier2') && icon
+			}
 		};
 	}
 
+	/**
+	 * Standard mappings for icons
+	 */
 	static get components() {
 		return {
-			'learning-path': html`<d2l-icon icon="tier1:exemption-add"></d2l-icon>`,
-			'course-offering': html`<d2l-icon icon="tier1:course"></d2l-icon>`,
-			default: html`<d2l-icon icon="tier1:quizzing"></d2l-icon>`
+			'learning-path': 'tier1:exemption-add',
+			'course-offering': 'tier1:course',
+			'user-assignment-activity': 'tier2:assignments',
+			'user-checklist-activity': 'tier2:checklist',
+			'user-content-activity': 'tier2:content',
+			'user-course-offering-activity-usage': 'tier2:syllabus',
+			'user-discussion-activity': 'tier2:discussions',
+			'user-quiz-activity': 'tier2:quizzing',
+			'user-quiz-attempt-activity': 'tier2:quizzing',
+			'user-survey-activity': 'tier2:surveys',
+			default: 'tier1:quizzing'
 		};
 	}
 
@@ -24,14 +43,26 @@ class ActivityIcon extends HypermediaStateMixin(LitElement) {
 
 	render() {
 		let icon = ActivityIcon.components.default;
-		this.classes.some(hmClass => {
-			if (!ActivityIcon.components[hmClass]) return;
-			icon = ActivityIcon.components[hmClass];
-			return true;
-		});
+		if (this.configuredIcon) {
+			icon = `tier2:${this.configuredIcon.properties.iconSetKey}`;
+		} else {
+			this.classes.some(hmClass => {
+				if (!ActivityIcon.components[hmClass]) return false;
+				icon = ActivityIcon.components[hmClass];
+				return true;
+			});
+		}
 		return html`
-			${icon}
+			<d2l-icon icon="${icon}" class="d2l-skeletize"></d2l-icon>
 		`;
+	}
+
+	get _loaded() {
+		return !this.skeleton;
+	}
+
+	set _loaded(loaded) {
+		this.skeleton = !loaded;
 	}
 }
 customElements.define('d2l-activity-icon', ActivityIcon);
