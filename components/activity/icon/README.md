bbcc443953c34bee5c1d8e6af618f95dff7b822b
Zina Ramirez <instinctualart@gmail.com>
2021-03-17 17:13:17 -0400

    feat: Updating the icon to include work to do widget effort (#143)
    * Updating the icon to include work to do widget effort
    
    * Making sure things are properly typed/private

--- components/activity/icon/README.md
+++ components/activity/icon/README.md
@@ -1,3 +1,16 @@
 # d2l-activity-icon
 
 Shows the `d2l-icon` associated with the activity based on its classes.
+Activities may also be configured to use a custom `tier2` icon with a sub-entity.
+
+## Tier 1 Standard Activity Icons
+
+These icons are typically used in lists.
+
+![Screenshot of standard icons](screenshots/activity-icon-screenshot.png)
+
+## Tier 2 User-specific Activity Icons
+
+These icons are used in Work-to-do widget and anywhere where the activity is specific to the user.
+
+![Screenshot of user-specific icons](screenshots/activity-icon-user-screenshot.png)
--- components/activity/icon/d2l-activity-icon.js
+++ components/activity/icon/d2l-activity-icon.js
@@ -1,37 +1,68 @@
 import '@brightspace-ui/core/components/icons/icon.js';
 import { html, LitElement } from 'lit-element/lit-element.js';
 import { HypermediaStateMixin, observableTypes } from '@brightspace-hmc/foundation-engine/framework/lit/HypermediaStateMixin.js';
+import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
 
-class ActivityIcon extends HypermediaStateMixin(LitElement) {
+/**
+ * Activity Icon foundation component that displays an icon when given an activity href.
+ * Looks for a subentity for configurable icon support. Defaults to standard mappings depending on the activity class
+ */
+class ActivityIcon extends SkeletonMixin(HypermediaStateMixin(LitElement)) {
 	static get properties() {
 		return {
-			classes: { type: Array, observable: observableTypes.classes }
+			_classes: { type: Array, observable: observableTypes.classes },
+			_configuredIcon: { type: Object, observable: observableTypes.subEntity, rel: 'icon',
+				method: (icon) => icon.class.includes('tier2') && icon
+			}
 		};
 	}
 
+	/**
+	 * Standard mappings for icons
+	 */
 	static get components() {
 		return {
-			'learning-path': html`<d2l-icon icon="tier1:exemption-add"></d2l-icon>`,
-			'course-offering': html`<d2l-icon icon="tier1:course"></d2l-icon>`,
-			default: html`<d2l-icon icon="tier1:quizzing"></d2l-icon>`
+			'learning-path': 'tier1:exemption-add',
+			'course-offering': 'tier1:course',
+			'user-assignment-activity': 'tier2:assignments',
+			'user-checklist-activity': 'tier2:checklist',
+			'user-content-activity': 'tier2:content',
+			'user-course-offering-activity-usage': 'tier2:syllabus',
+			'user-discussion-activity': 'tier2:discussions',
+			'user-quiz-activity': 'tier2:quizzing',
+			'user-quiz-attempt-activity': 'tier2:quizzing',
+			'user-survey-activity': 'tier2:surveys',
+			default: 'tier1:quizzing'
 		};
 	}
 
 	constructor() {
 		super();
-		this.classes = [];
+		this._classes = [];
 	}
 
 	render() {
 		let icon = ActivityIcon.components.default;
-		this.classes.some(hmClass => {
-			if (!ActivityIcon.components[hmClass]) return;
-			icon = ActivityIcon.components[hmClass];
-			return true;
-		});
+		if (this._configuredIcon) {
+			icon = `tier2:${this._configuredIcon.properties.iconSetKey}`;
+		} else {
+			this._classes.some(hmClass => {
+				if (!ActivityIcon.components[hmClass]) return false;
+				icon = ActivityIcon.components[hmClass];
+				return true;
+			});
+		}
 		return html`
-			${icon}
+			<d2l-icon icon="${icon}" class="d2l-skeletize"></d2l-icon>
 		`;
+	}
+
+	get _loaded() {
+		return !this.skeleton;
+	}
+
+	set _loaded(loaded) {
+		this.skeleton = !loaded;
 	}
 }
 customElements.define('d2l-activity-icon', ActivityIcon);
