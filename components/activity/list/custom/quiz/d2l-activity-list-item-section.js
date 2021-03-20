5fc17c241e2d4676c7977c03ea63125b19823c34
Alex Li <Alex.Li@D2L.com>
2021-03-19 22:18:30 -0700

    US124813: display sections with their content.

--- components/activity/list/custom/quiz/d2l-activity-list-item-section.js
+++ components/activity/list/custom/quiz/d2l-activity-list-item-section.js
@@ -9,14 +9,21 @@ import { customHypermediaElement, html } from '@brightspace-hmc/foundation-engin
 import { HypermediaStateMixin, observableTypes } from '@brightspace-hmc/foundation-engine/framework/lit/HypermediaStateMixin.js';
 import { LocalizeQuizEditor } from './lang/localization.js';
 import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
+import { repeat } from 'lit-html/directives/repeat';
 
 const rels = Object.freeze({
-	specialization: 'https://api.brightspace.com/rels/specialization'
+	specialization: 'https://api.brightspace.com/rels/specialization',
+	collection: 'https://activities.api.brightspace.com/rels/activity-collection',
+	item: 'item'
 });
+
 const route = {
 	specialization:
-		{ observable: observableTypes.link, rel: rels.specialization }
+		{ observable: observableTypes.link, rel: rels.specialization },
+	collection:
+		{ observable: observableTypes.link, rel: rels.collection }
 };
+
 const componentClass = class extends SkeletonMixin(HypermediaStateMixin(LocalizeQuizEditor(LitElement))) {
 	static get properties() {
 		return {
@@ -31,6 +38,12 @@ const componentClass = class extends SkeletonMixin(HypermediaStateMixin(Localize
 				observable: observableTypes.property,
 				id: 'typeText',
 				route: [route.specialization]
+			},
+			items: {
+				type: Array,
+				observable: observableTypes.subEntities,
+				rel: rels.item,
+				route: [route.collection]
 			}
 		};
 	}
@@ -50,6 +63,7 @@ const componentClass = class extends SkeletonMixin(HypermediaStateMixin(Localize
 					display: flex;
 					flex-wrap: nowrap;
 					width: 100%;
+					height: 2rem;
 				}
 				.checkbox > * {
 					display: inline;
@@ -64,21 +78,32 @@ const componentClass = class extends SkeletonMixin(HypermediaStateMixin(Localize
 					margin-inline-start: 2rem;
 					max-width: 10rem;
 				}
+				.section-nested-items {
+					margin-left: 2rem
+				}
 			`];
 	}
 
 	constructor() {
 		super();
 		this.skeleton = true;
+		this.items = [];
 	}
 
 	render() {
+		// It is just a temporary solution to add nested list here. Waiting for the decision/discusson on how
+		// to do nested lists properly
 		return html`
 			<div class="section-item d2l-skeletize">
 				<div class="checkbox"><d2l-input-checkbox></d2l-input-checkbox></div>
 				<div class="section"><span class="d2l-heading-2">${this.name}</span></div>
 			</div>
 			<div class="d2l-body-small section-type d2l-skeletize">${this.typeText}</div>
+			<d2l-list separators="none" class="section-nested-items" @d2l-list-item-position-change="${this._moveItems}">
+				${repeat(this.items, item => item.href, (item, idx) => html`
+					<d2l-activity-collection-item-quiz number="${idx + 1}" href="${item.href}" .token="${this.token}" key="${item.properties.id}"></d2l-activity-collection-item-quiz>
+				`)}
+			</d2l-list>
 		`;
 	}
 
