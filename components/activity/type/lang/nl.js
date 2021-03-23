e99dd2037c6f56e8f8cc28dfb321c9cf30f2bbbd
JenkinsReader <resupport@desire2learn.com>
2021-03-23 12:11:06 -0400

    Translation Updates

--- components/activity/type/lang/nl.js
+++ components/activity/type/lang/nl.js
@@ -1,10 +1,10 @@
 /* eslint quotes: 0 */
 
 export default {
-	"label-activity": "Activiteit",
-	"label-course": "Cursus",
-	"label-group": "Groepsopdracht",
-	"label-groupCategory": "Groepscategorie",
-	"label-individual": "Individuele opdracht",
-	"label-learningPath": "Leertraject"
+	"label-activity": "$$$$$$$$$$$$$",
+	"label-course": "$$$$$$$$",
+	"label-group": "$$$$$$$$$$$$$$$$$$",
+	"label-groupCategory": "$$$$$$$$$$$$$$$$$$$$",
+	"label-individual": "$$$$$$$$$$$$$$ $$$$$$$$$$",
+	"label-learningPath": "$$$$$$$$$$$$$$"
 };
--- components/common/lang/nl.js
+++ components/common/lang/nl.js
@@ -1,5 +1,5 @@
 /* eslint quotes: 0 */
 
 export default {
-	name: "Onderdeel wordt geladen" // Name of a course, a user or anything type of name/title of something.
+	name: "$$$$$$$$$$$$ $$$$$$$ $$$$$$$$$" // Name of a course, a user or anything type of name/title of something.
 };
