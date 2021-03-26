b2ee62a1129112bbbae0a4191254f164652c71c3
Maya Jones <m6jones@users.noreply.github.com>
2021-03-26 16:26:07 -0400

    LangFix (#158)

--- components/activity/editor/lang/ko.js
+++ components/activity/editor/lang/ko.js
@@ -8,7 +8,7 @@ export default {
 	"label-instructions": "지시사항", // Shows where the user should write instructions for an activity
 	"label-ok": "확인", // Confirm dialog box was read to return to editing learning path after save error
 	"noActivitiesInCollection": "시작하려면 활동 추가를 클릭합니다", // Displayed when the collection is loaded and contains no activities
-	"numberOfActivities": "{Count,plural,=1{1개 활동} 기타 {{count}개 활동}}", // The number of learning tasks currently in the list.
+	"numberOfActivities": "{count,plural,=1{1개 활동} 기타 {{count}개 활동}}", // The number of learning tasks currently in the list.
 	"text-activities": "활동", // The following is a list of tasks to be completed to finish the learning path
 	"text-editDialogSaveTitle": "학습 경로 변경 사항이 저장되지 않았습니다", // Error when the edits to the learning path could not be saved
 	"text-editDialogSaveContent": "이 학습 경로에 대한 변경 사항을 저장할 수 없습니다. 확인을 누른 후 다시 시도할 수 있습니다.", // Message telling user changes could not be saved. Informs user they can try to save again after pressing ok to close dialog box
