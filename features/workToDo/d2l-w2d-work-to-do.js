6659765344cfbc5e56f9e5632f9ad2c2e2ea3c1b
Maya Jones <Maya.Jones@D2L.com>
2021-03-30 13:34:09 -0400

    Some updates to show of another PR

--- features/workToDo/d2l-w2d-work-to-do.js
+++ features/workToDo/d2l-w2d-work-to-do.js
@@ -1,17 +1,52 @@
 import { html, LitElement } from 'lit-element/lit-element.js';
 import { HypermediaStateMixin, observableTypes } from '@brightspace-hmc/foundation-engine/framework/lit/HypermediaStateMixin.js';
-import { W2DSirenClasses } from './w2dDateCategoryObserver.js';
+import { formatDate } from '@brightspace-ui/intl/lib/dateTime.js';
+import { W2dDateCategory } from './w2dDateCategoryObserver.js';
+
+const rel = Object.freeze({
+	myActivities: 'https://activities.api.brightspace.com/rels/my-activities'
+});
 
 class w2dWorkToDo extends HypermediaStateMixin(LitElement) {
 	static get properties() {
 		return {
-			_entity: { type: Object, observable: observableTypes.entity },
-			_classes: { type: String, observable: observableTypes.custom, observableObject: W2DSirenClasses, method: (classes) => classes.pop() }
+			currentTime: { type: String, attribute: 'current-time' },
+			groupByDays: { type: Number, attribute: 'group-by-days' },
+			_categories: {
+				type: Object,
+				observable: observableTypes.custom,
+				observableObject: W2dDateCategory,
+				groupByDays: 'groupByDays',
+				startDate: 'currentTime',
+				route: [
+					{ observable: observableTypes.link, rel: rel.myActivities }
+				],
+				method: (categories) => Object.values(categories)
+			},
+			_myActivitiesHref: { type: String, observable: observableTypes.link, rel: rel.myActivities }
 		};
 	}
+
+	constructor() {
+		super();
+		this._categories = [];
+		this.waitForProperty('currentTime');
+		this.waitForProperty('groupByDays');
+	}
+
 	render() {
-		if (typeof this._entity === 'object') return html`${this._classes}<pre>${JSON.stringify(this._entity.toJSON(), undefined, 4)}</pre>`;
-		return null;
+		const categories = this._categories.map(category => {
+			const endDateString = category.endDate.getTime() !== category.startDate.getTime() ? html` - ${formatDate(category.endDate, { format: 'monthDay' })}` : null;
+			return html`
+				<h2>${formatDate(category.startDate, { format: 'monthDay' })}${endDateString}</h2>
+				<ul>
+					<li>${this._myActivitiesHref}</li>
+					<li>${category.index}</li>
+					<li>${category.count}</li>
+				</ul>
+			`;
+		});
+		return html`${categories}`;
 	}
 
 }
--- features/workToDo/w2dDateCategoryObserver.js
+++ features/workToDo/w2dDateCategoryObserver.js
@@ -1,17 +1,96 @@
-import { Observable } from '@brightspace-hmc/foundation-engine/state/observable/Observable.js';
+import { SirenSubEntities } from '@brightspace-hmc/foundation-engine/state/observable/SirenSubEntities.js';
 
-export class W2DSirenClasses extends Observable {
-	get classes() {
-		return this._observers.value;
+const msInADay = 86400000;
+
+export class W2dDateCategory extends SirenSubEntities {
+
+	static definedProperty({ token, groupByDays, startDate, category, verbose }) {
+		return { token, groupByDays, startDate, category, verbose };
 	}
 
-	set classes(value) {
-		if (this.classes !== value) {
-			this._observers.setProperty(value);
+	constructor({ id, token, state, verbose }) {
+		super({id, token, state, verbose});
+	}
+
+	/**
+	 * @return { Array<SirenFacade> }
+	 */
+	get entities() {
+		return this._observers.value || {};
+	}
+
+	/**
+	 * @param { Array<SirenFacade> } sirenFacades - List of SirenFacade objects
+	 */
+	set entities(sirenFacades) {
+		if (!this._startDate || !this._groupByDays) {
+			this._sirenFacades = sirenFacades;
+			return;
+		}
+		const sirenFacadesByCategory = {};
+		const categoryInfo = {};
+		sirenFacades.forEach(sirenFacade => {
+			const date = dueDate(sirenFacade, this._startDate);
+			const index = Math.floor(date.daysPassed / this._groupByDays);
+			if (!categoryInfo[index]) {
+				const epochStartTime = this._startDate.getTime();
+				const startDate = new Date(epochStartTime + index * msInADay * this._groupByDays);
+				const endDate = new Date(startDate.getTime() +  msInADay * (this._groupByDays - 1));
+				categoryInfo[index] = {
+					startDate,
+					endDate,
+					index
+				};
+			}
+			sirenFacadesByCategory[index] = sirenFacadesByCategory[index] ? sirenFacadesByCategory[index] : [];
+			sirenFacadesByCategory[index].push(date);
+		});
+		Object.keys(categoryInfo).forEach(index => {
+			categoryInfo[index].count = sirenFacadesByCategory[index].length;
+		});
+		if (this.entities !== sirenFacades) {
+			this._observers.setProperty({categoryInfo, sirenFacadesByCategory} || []);
 		}
 	}
 
-	setSirenEntity(sirenEntity) {
-		this.classes = sirenEntity && sirenEntity['class'];
+	// Note to self:
+	// Make it so we can wait on attributes to be set by the UI before running the HREF
+
+	addObserver(observer, property, { method, category, startDate, groupByDays } = {}) {
+		if (!category) {
+			this._startDate = new Date(observer[startDate]);
+			this._groupByDays = observer[groupByDays];
+			if (this._sirenFacades) this.entities = this._sirenFacades;
+		}
+
+		const filterByCategory = (categoryMap) => {
+			if (!category) {
+				return categoryMap.categoryInfo;
+			}
+
+			return categoryMap.sirenFacadesByCategory;
+		};
+
+		const newMethod = method ? (items) => method(filterByCategory(items)) : filterByCategory;
+		super.addObserver(observer, property, { method: newMethod });
 	}
+}
+
+function dueDate(sirenFacade, relativeTime) {
+	const entities = sirenFacade.entities.filter(entity => entity.hasClass('due-date'));
+	if (!entities.length) return false;
+
+	const entity = entities.pop();
+
+	const date = new Date(Date.parse(entity.properties.localizedDate));
+
+	return {
+		dateObject: date,
+		year: date.getFullYear(),
+		month: date.getMonth(),
+		day: date.getDay(),
+		date: date.getDate(),
+		time: date.getTime(),
+		daysPassed: (Math.floor(date.getTime() / msInADay) - Math.floor(relativeTime.getTime() / msInADay))
+	};
 }
