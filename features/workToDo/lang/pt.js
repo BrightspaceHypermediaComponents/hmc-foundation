edaaa4fab0e03d5edbdf40c45d39f84ef14ca300
Ten Bradley <Ten.Bradley@D2L.com>
2021-03-31 15:10:02 -0400

    fixed format of lang files

--- features/workToDo/lang/pt.js
+++ features/workToDo/lang/pt.js
@@ -1,28 +1,28 @@
 export const val = {
-  activitiesAvailable: "As atividades que vencem ou encerram em duas semanas estão concluídas! Marque Exibir todos os trabalhos para ver o que está por vir.", // 'Empty View' - When widget has no activities to display within the next two weeks, but there are more activities further into the future that can be shown on the full screen view
-  allClear: "Tudo certo por enquanto!", // Displayed as header line in widget text when there are no activities
-  assignment: "Atividade",  // Meta-data descriptor that informs which type of activity is being displayed on a line item
-  backToD2L: "Voltar ao Início", // Displayed in the immersive navbar to escape out of fullscreen view
-  checklist: "Lista de verificação", // Meta-data descriptor that informs which type of activity is being displayed on a line item
-  comeBackNoFutureActivities: "Volte mais tarde para verificar se você tem trabalho pendente.", // 'Empty state' - When there are no activities in full page view
-  content: "Conteúdo", // Meta-data descriptor that informs which type of activity is being displayed on a line item
-  course: "Curso", // Meta-data descriptor that informs which type of activity is being displayed on a line item
-  dateHeader: "{startDay} de {startMonth} – {endDay} de {endMonth}", // Indicates that the below list of activities are due/end within the listed date range
-  discussion: "Discussão", // Meta-data descriptor that informs which type of activity is being displayed on a line item
-  fullViewLink: "Exibir todos os trabalhos", // Link text displayed in "Activities View", where the user can navigate to the full page view to see all work
-  goToDiscover: "Ir para o Discover", // Button text displayed in 'Empty View' when user can navigate to discover homepage from the widget
-  loadMore: "Carregar mais", // Button text displayed in 'Fullscreen View' that allows the user to access the next page of activities which will append to the bottom of the list currently shown
-  loadMoreDescription: "Exibir mais atividades atribuídas", // Additional description text to accompany the load more button for additional clarity for the user
-  workToDo: "Trabalho pendente", // Widget title
-  noActivities: "Você não tem atividades incompletas com prazos ou datas finais disponíveis.", // 'Empty state' - When widget has no activities in full page view
-  noActivitiesFutureActivities: "Você não tem atividades incompletas que vencem ou encerram em breve. Volte mais tarde ou selecione Exibir todos os trabalhos para ver o que está por vir.",  // 'Empty View' - When widget has no activities to display within the next few weeks & there are activities in the future
-  noActivitiesNoFutureActivities: "Você não tem atividades incompletas com prazos ou datas finais disponíveis. Volte mais tarde para verificar se você tem trabalho pendente.", // 'Empty View' - When widget has no activities to display within the next few weeks & there are no more activities in the future
-  nothingHere: "Não há nada aqui…", // Displayed as header line in widget text when there are no activities within the provided time period
-  overdue: "Atraso", // Indicates that the below list of activities are overdue (have a due date that is in the past)
-  quiz: "Questionário", // Meta-data descriptor that informs which type of activity is being displayed on a line item
-  StartsWithDate: "Início: {startDate}", // show Start Date on status indicator when an activity starts in the future - formatted like "Starts Aug 15"
-  survey: "Pesquisa", // Meta-data descriptor that informs which type of activity is being displayed on a line item
-  upcoming: "Próximos trabalhos", // Indicates that the below list of activites are upcoming (have a due due or end date that is in the future)
-  viewAllWork: "Exibir todos os trabalhos", // Button text displayed in 'Empty View' when user can navigate to full page view to see all work
-  xWeeksClear: "{count, plural, =1 {1 semana} other {{count} semanas}} sem trabalhos a fazer!" // 'Empty state' - Header when widget has no activities to display within the next x weeks
-}
+	activitiesAvailable: 'As atividades que vencem ou encerram em duas semanas estão concluídas! Marque Exibir todos os trabalhos para ver o que está por vir.', // 'Empty View' - When widget has no activities to display within the next two weeks, but there are more activities further into the future that can be shown on the full screen view
+	allClear: 'Tudo certo por enquanto!', // Displayed as header line in widget text when there are no activities
+	assignment: 'Atividade',  // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	backToD2L: 'Voltar ao Início', // Displayed in the immersive navbar to escape out of fullscreen view
+	checklist: 'Lista de verificação', // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	comeBackNoFutureActivities: 'Volte mais tarde para verificar se você tem trabalho pendente.', // 'Empty state' - When there are no activities in full page view
+	content: 'Conteúdo', // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	course: 'Curso', // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	dateHeader: '{startDay} de {startMonth} – {endDay} de {endMonth}', // Indicates that the below list of activities are due/end within the listed date range
+	discussion: 'Discussão', // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	fullViewLink: 'Exibir todos os trabalhos', // Link text displayed in "Activities View", where the user can navigate to the full page view to see all work
+	goToDiscover: 'Ir para o Discover', // Button text displayed in 'Empty View' when user can navigate to discover homepage from the widget
+	loadMore: 'Carregar mais', // Button text displayed in 'Fullscreen View' that allows the user to access the next page of activities which will append to the bottom of the list currently shown
+	loadMoreDescription: 'Exibir mais atividades atribuídas', // Additional description text to accompany the load more button for additional clarity for the user
+	workToDo: 'Trabalho pendente', // Widget title
+	noActivities: 'Você não tem atividades incompletas com prazos ou datas finais disponíveis.', // 'Empty state' - When widget has no activities in full page view
+	noActivitiesFutureActivities: 'Você não tem atividades incompletas que vencem ou encerram em breve. Volte mais tarde ou selecione Exibir todos os trabalhos para ver o que está por vir.',  // 'Empty View' - When widget has no activities to display within the next few weeks & there are activities in the future
+	noActivitiesNoFutureActivities: 'Você não tem atividades incompletas com prazos ou datas finais disponíveis. Volte mais tarde para verificar se você tem trabalho pendente.', // 'Empty View' - When widget has no activities to display within the next few weeks & there are no more activities in the future
+	nothingHere: 'Não há nada aqui…', // Displayed as header line in widget text when there are no activities within the provided time period
+	overdue: 'Atraso', // Indicates that the below list of activities are overdue (have a due date that is in the past)
+	quiz: 'Questionário', // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	StartsWithDate: 'Início: {startDate}', // show Start Date on status indicator when an activity starts in the future - formatted like "Starts Aug 15"
+	survey: 'Pesquisa', // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	upcoming: 'Próximos trabalhos', // Indicates that the below list of activites are upcoming (have a due due or end date that is in the future)
+	viewAllWork: 'Exibir todos os trabalhos', // Button text displayed in 'Empty View' when user can navigate to full page view to see all work
+	xWeeksClear: '{count, plural, =1 {1 semana} other {{count} semanas}} sem trabalhos a fazer!' // 'Empty state' - Header when widget has no activities to display within the next x weeks
+};
