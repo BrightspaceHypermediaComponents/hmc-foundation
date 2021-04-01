32895aee0dff62cf74ec0ff478ce6ced68ac49c2
Ten Bradley <69812595+tenbradley@users.noreply.github.com>
2021-04-01 11:43:18 -0400

    feat: added work to do lang terms from activities repo (#163)
    * added work to do lang terms from activities repo
    
    * fixed format of lang files

---
+++ features/workToDo/lang/tr.js
@@ -0,0 +1,28 @@
+export const val = {
+	activitiesAvailable: 'Teslim tarihi veya sona erme tarihi iki hafta içinde olan etkinlikler tamamlandı! Daha sonraki etkinlikleri görmek için Tüm İşleri Görüntüle seçeneğini işaretleyin.', // 'Empty View' - When widget has no activities to display within the next two weeks, but there are more activities further into the future that can be shown on the full screen view
+	allClear: 'Şimdilik Her Şey Tamamlandı!', // Displayed as header line in widget text when there are no activities
+	assignment: 'Ödev',  // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	backToD2L: 'Ana Sayfaya Geri Dön', // Displayed in the immersive navbar to escape out of fullscreen view
+	checklist: 'Kontrol Listesi', // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	comeBackNoFutureActivities: 'Üzerinde çalışacağınız bir iş olup olmadığını görmek için daha sonra tekrar ziyaret edin.', // 'Empty state' - When there are no activities in full page view
+	content: 'İçerik', // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	course: 'Ders', // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	dateHeader: '{startDay} {startMonth} - {endDay} {endMonth}', // Indicates that the below list of activities are due/end within the listed date range
+	discussion: 'Tartışma', // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	fullViewLink: 'Tüm çalışmaları görüntüle', // Link text displayed in "Activities View", where the user can navigate to the full page view to see all work
+	goToDiscover: "Keşfet'e Git", // Button text displayed in 'Empty View' when user can navigate to discover homepage from the widget
+	loadMore: 'Daha Fazla Yükle', // Button text displayed in 'Fullscreen View' that allows the user to access the next page of activities which will append to the bottom of the list currently shown
+	loadMoreDescription: 'Daha fazla atanmış etkinlik görüntüle', // Additional description text to accompany the load more button for additional clarity for the user
+	workToDo: 'Yapılacak İşler', // Widget title
+	noActivities: 'Teslim veya sona erme tarihi bulunan tamamlanmamış etkinliğiniz yok.', // 'Empty state' - When widget has no activities in full page view
+	noActivitiesFutureActivities: 'Teslim veya sona erme tarihi yaklaşmakta olan tamamlanmamış etkinliğiniz yok. Daha sonraki etkinlikleri görmek için daha sonra tekrar ziyaret edin veya Tüm İşleri Görüntüleyin.',  // 'Empty View' - When widget has no activities to display within the next few weeks & there are activities in the future
+	noActivitiesNoFutureActivities: 'Teslim veya sona erme tarihi bulunan tamamlanmamış etkinliğiniz yok. Üzerinde çalışacağınız bir iş olup olmadığını görmek için daha sonra tekrar ziyaret edin.', // 'Empty View' - When widget has no activities to display within the next few weeks & there are no more activities in the future
+	nothingHere: 'Hiçbir şey yok...', // Displayed as header line in widget text when there are no activities within the provided time period
+	overdue: 'Süresi Dolmuş', // Indicates that the below list of activities are overdue (have a due date that is in the past)
+	quiz: 'Sınav', // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	StartsWithDate: 'Başlama Tarihi: {startDate}', // show Start Date on status indicator when an activity starts in the future - formatted like "Starts Aug 15"
+	survey: 'Anket', // Meta-data descriptor that informs which type of activity is being displayed on a line item
+	upcoming: 'Yaklaşan İş', // Indicates that the below list of activites are upcoming (have a due due or end date that is in the future)
+	viewAllWork: 'Tüm İşleri Görüntüle', // Button text displayed in 'Empty View' when user can navigate to full page view to see all work
+	xWeeksClear: '{count, plural, =1 {1 hafta} other {{count} hafta}} tamamlandı!' // 'Empty state' - Header when widget has no activities to display within the next x weeks
+};
