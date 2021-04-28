3774853bbfbeec225b50638ee65d841b1efc63ef
Adrian Logan <adrian.logan@D2L.com>
2021-04-28 16:18:40 -0400

    Add d2l lti activity component Add sekeleton support to hc-description

--- components/common/d2l-hc-description.js
+++ components/common/d2l-hc-description.js
@@ -2,7 +2,9 @@ import { css, LitElement } from 'lit-element/lit-element.js';
 import { HypermediaStateMixin, observableTypes} from '@brightspace-hmc/foundation-engine/framework/lit/HypermediaStateMixin.js';
 import { html } from '@brightspace-hmc/foundation-engine/framework/lit/hypermedia-components.js';
 
-class HmDescription extends HypermediaStateMixin(LitElement) {
+import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
+
+class HmDescription extends SkeletonMixin(HypermediaStateMixin(LitElement)) {
 	static get properties() {
 		return {
 			description: { type: String, observable: observableTypes.property }
@@ -10,13 +12,30 @@ class HmDescription extends HypermediaStateMixin(LitElement) {
 	}
 
 	static get styles() {
-		return [ css`` ];
+		return [ super.styles, css`
+			.skeleton-placeholder {
+				width: 5rem;
+				height: 1rem;
+			}
+		`];
+	}
+
+	constructor() {
+		super();
+		this.skeleton = true;
 	}
 
 	render() {
-		return html`
-			${this.description}
-		`;
+		return html`${this.description ? html`<span class="d2l-skeletize">${this.description}</span>` : 
+				html`${this.skeleton ? html`<div class="d2l-skeletize skeleton-placeholder"></div>` : html``}`}`;
+	}
+
+	get _loaded() {
+		return !this.skeleton;
+	}
+
+	set _loaded(loaded) {
+		this.skeleton = !loaded;
 	}
 
 }
