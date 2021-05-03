27c25e65f324d08b62e532dee86b4d4018de5079
Maya Jones <Maya.Jones@D2L.com>
2021-05-03 11:29:23 -0400

    ten test

--- features/workToDo/w2dSummonActionObserver.js
+++ features/workToDo/w2dSummonActionObserver.js
@@ -18,6 +18,7 @@ export class W2dSummonAction extends SirenSummonAction {
 			});
 		}
 		super.addObserver(observer, property, { method, route });
+		this._setPage(observer[page]);
 	}
 
 	get method() {
@@ -41,6 +42,18 @@ export class W2dSummonAction extends SirenSummonAction {
 		this._fields = this._decodeFields(this._rawSirenAction);
 		this._method = this._rawSirenAction.method;
 		if (this._routes.size > 0) {
+			this.routedState = await this.createRoutedState(this.href, this._token.rawToken);
+			this._routes.forEach((route, observer) => {
+				this.routedState.addObservables(observer, route);
+			});
+			fetch(this.routedState);
+		}
+	}
+
+	async _setPage(page) {
+		if (this._page === page) return;
+		this._page = page;
+		if (this._routes.size > 0 && this._href && this._token) {
 			this.routedState = await this.createRoutedState(this.href, this._token.rawToken);
 			this._routes.forEach((route, observer) => {
 				this.routedState.addObservables(observer, route);
