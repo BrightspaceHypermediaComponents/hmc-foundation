3fee5190414cbdab9017608f53e37771fed5b6d5
Adrian Logan <adrian.logan519@gmail.com>
2021-05-03 21:44:38 -0400

    Add d2l lti activity component (#198)
    * Add d2l lti activity component
    Add sekeleton support to hc-description
    
    * Add allow options to iframe and update readme to fix tags.
    
    * Fix lint issues and make description private in d2l-hc-description

--- components/common/d2l-hc-description.js
+++ components/common/d2l-hc-description.js
@@ -2,21 +2,40 @@ import { css, LitElement } from 'lit-element/lit-element.js';
 import { HypermediaStateMixin, observableTypes} from '@brightspace-hmc/foundation-engine/framework/lit/HypermediaStateMixin.js';
 import { html } from '@brightspace-hmc/foundation-engine/framework/lit/hypermedia-components.js';
 
-class HmDescription extends HypermediaStateMixin(LitElement) {
+import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
+
+class HmDescription extends SkeletonMixin(HypermediaStateMixin(LitElement)) {
 	static get properties() {
 		return {
-			description: { type: String, observable: observableTypes.property }
+			_description: { type: String, observable: observableTypes.property }
 		};
 	}
 
 	static get styles() {
-		return [ css`` ];
+		return [ super.styles, css`
+			.skeleton-placeholder {
+				width: 5rem;
+				height: 1rem;
+			}
+		`];
+	}
+
+	constructor() {
+		super();
+		this.skeleton = true;
 	}
 
 	render() {
-		return html`
-			${this.description}
-		`;
+		return html`${this._description ? html`<span class="d2l-skeletize">${this._description}</span>` :
+			html`${this.skeleton ? html`<div class="d2l-skeletize skeleton-placeholder"></div>` : html``}`}`;
+	}
+
+	get _loaded() {
+		return !this.skeleton;
+	}
+
+	set _loaded(loaded) {
+		this.skeleton = !loaded;
 	}
 
 }
---
+++ features/lti/d2l-lti-activity.js
@@ -0,0 +1,140 @@
+import '../../components/common/d2l-hc-description.js';
+import '../../components/common/d2l-hc-name.js';
+import '@brightspace-ui/core/components/button/button.js';
+import '@brightspace-ui/core/components/button/button-subtle.js';
+import { bodyCompactStyles, bodySmallStyles, heading3Styles } from '@brightspace-ui/core/components/typography/styles.js';
+import { css, LitElement } from 'lit-element/lit-element.js';
+import { customHypermediaElement, html } from '@brightspace-hmc/foundation-engine/framework/lit/hypermedia-components.js';
+import { HypermediaStateMixin, observableTypes } from '@brightspace-hmc/foundation-engine/framework/lit/HypermediaStateMixin.js';
+import { LocalizeDynamicMixin } from '@brightspace-ui/core/mixins/localize-dynamic-mixin.js';
+import { SkeletonMixin } from '@brightspace-ui/core/components/skeleton/skeleton-mixin.js';
+
+const rels = Object.freeze({
+	linkPlacement: 'https://lti.api.brightspace.com/rels/link-placement'
+});
+
+class LtiActivity extends SkeletonMixin(LocalizeDynamicMixin(HypermediaStateMixin(LitElement))) {
+
+	static get properties() {
+		return {
+			iFrameWidth: {
+				type: String,
+				observable: observableTypes.property,
+				route: [ {
+					observable: observableTypes.link,
+					rel: rels.linkPlacement
+				}]
+			},
+			iFrameHeight: {
+				type: String,
+				observable: observableTypes.property,
+				route: [ {
+					observable: observableTypes.link,
+					rel: rels.linkPlacement
+				}]
+			},
+			launchUrl: {
+				type: String,
+				observable: observableTypes.property,
+				route: [ {
+					observable: observableTypes.link,
+					rel: rels.linkPlacement
+				}]
+			},
+			_ltiLinkHref: {
+				type: String,
+				observable: observableTypes.link,
+				rel: rels.linkPlacement
+			},
+			_showOpenInNewWindowButton: {
+				type: Boolean,
+				attribute: 'open-as-external'
+			}
+		};
+	}
+
+	static get styles() {
+		return [ super.styles, bodySmallStyles, heading3Styles, bodyCompactStyles, css`
+		  :host {
+			display: inline-block;
+
+		  }
+		  .spanning-button {
+			width: 100%;
+		  }
+		  .skeleton-placeholder {
+			  width: 10rem;
+			  height: 5rem;
+		  }
+		` ];
+	}
+
+	static get localizeConfig() {
+		return {
+			importFunc: async lang => (await import(`./lang/${lang}.js`)).default
+		};
+	}
+
+	constructor() {
+		super();
+		this._showOpenInNewWindowButton = false;
+		this.skeleton = true;
+	}
+
+	render() {
+		return html`
+			<div>
+				<d2l-hc-name ?skeleton=${this.skeleton} class="d2l-heading-3" href="${this._ltiLinkHref}" .token="${this.token}"></d2l-hc-name>
+			</div>
+			<div class="d2l-body-compact">
+				<d2l-hc-description  ?skeleton=${this.skeleton} href="${this._ltiLinkHref}" .token="${this.token}"></d2l-hc-description>
+			</div>
+			<div class="d2l-body-compact">
+				${this.localize('external-activity-check-below')}
+			</div>
+
+			${this.skeleton ? html`<div class="d2l-skeletize skeleton-placeholder"></div>` :
+
+		html`${this._showOpenInNewWindowButton ?
+			html`<d2l-button class="spanning-button" primary @click="${this._onOpenInNewWindowClick}">${this.localize('open-in-new-window')}</d2l-button>` :
+			html`<iframe allow="microphone *; camera *; autoplay *" width="${this.iFrameWidth}" height="${this.iFrameHeight}" src="${this.launchUrl}"></iframe>
+						<div>
+							<d2l-button-subtle text="${this.localize('not-working-button')}" icon="tier1:smallscreen" icon-right @click="${this._onNotWorkingButtonClick}"></d2l-button-subtle>
+						</div>`
+
+		}`
+}								
+	  	`;
+	}
+	openWindow(url, title, width, height) {
+		const left = (screen.width / 2) - (width / 2);
+		const top = (screen.height / 2) - (height / 2);
+
+		const newWindow = window.open(url, title, `resizable=yes, width=${  width	 }, height=${  height  }, top=${ top  }, left=${  left}`);
+		newWindow.moveTo(top, left);
+		if (window.focus) newWindow.focus();
+	}
+	get _loaded() {
+		return !this.skeleton;
+	}
+
+	set _loaded(loaded) {
+		this.skeleton = !loaded;
+	}
+
+	_onNotWorkingButtonClick() {
+		this._showOpenInNewWindowButton = true;
+	}
+
+	_onOpenInNewWindowClick() {
+		this.openWindow(this.launchUrl, this.localize('external-activity'), this.iFrameWidth, this.iFrameHeight);
+	}
+
+}
+
+customHypermediaElement(
+	'd2l-lti-activity',
+	LtiActivity,
+	'd2l-activity-editor-main',
+	[['activity-usage', 'lti-activity-usage']]
+);
