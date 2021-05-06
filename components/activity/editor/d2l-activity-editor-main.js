10bfde32d09058228b1cb347e36595af73cafeba
Adrian Logan <adrian.logan@D2L.com>
2021-05-06 11:07:31 -0400

    fix: remove extension of d2l-activity-editor-main and change the Not Working button to be an open in new window button.

--- components/activity/editor/d2l-activity-editor-main.js
+++ components/activity/editor/d2l-activity-editor-main.js
@@ -1,5 +1,4 @@
 // START custom component imports
 import './assignment/d2l-activity-editor-main-assignment.js';
 import './collection/d2l-activity-editor-main-collection.js';
-import '../../../features/lti/d2l-lti-activity.js';
 // END custom component imports
