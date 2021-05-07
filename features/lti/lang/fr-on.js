23975c8fdc2c39db7510d5ac6f541b7bb6638a0c
Ten Bradley <69812595+tenbradley@users.noreply.github.com>
2021-05-07 13:16:44 -0400

    Delete fr-on.js
    Not translated to french. Removing so we don't forget to add fr-on once this is translated.

--- features/lti/lang/fr-on.js
+++
@@ -1,7 +0,0 @@
-/* eslint quotes: 0 */
-export default {
-	"external-activity-check-below": "This is an external activity. Please, check it below.",
-	"external-activity": "External Activity",
-	"not-working-button": "Not working?", // Button to switch from launching inline to open in new window
-	"open-in-new-window": "Open in New Window"
-};
