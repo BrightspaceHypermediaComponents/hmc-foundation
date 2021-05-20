1d723caa7419ccf28e7dc48adf25412a20bc249d
Zina Ramirez <instinctualart@gmail.com>
2021-05-20 12:18:23 -0400

    Rename to be more specific

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,9 +1,9 @@
-import handleRequest from './features/discover/demo/discover-api.js';
+import handleRequestDiscover from './features/discover/demo/discover-api.js';
 import http from 'http';
 import proxy from 'koa-proxies';
 
 const server = http.createServer((request, response) => {
-	handleRequest(request, response);
+	handleRequestDiscover(request, response);
 });
 
 server.listen(9000);
