e79b729c27684aaf40ec67448fc0053df0ec8fec
Bradley Hamelin <Bradley.Hamelin@D2L.com>
2021-07-06 13:48:14 -0400

    Revert card changes to make the PR do just one thing

--- features/discover/d2l-discover-rule-card.js
+++ features/discover/d2l-discover-rule-card.js
@@ -5,7 +5,6 @@ import '@brightspace-ui/core/components/dropdown/dropdown-menu.js';
 import '@brightspace-ui/core/components/menu/menu.js';
 import '@brightspace-ui/core/components/menu/menu-item.js';
 import './d2l-discover-rule-picker-dialog.js';
-import './d2l-discover-rule-card-match-info.js';
 import { css, html, LitElement } from 'lit-element/lit-element.js';
 import { LocalizeDynamicMixin } from '@brightspace-ui/core/mixins/localize-dynamic-mixin.js';
 import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
@@ -71,7 +70,8 @@ class RuleCard extends LocalizeDynamicMixin(RtlMixin(LitElement)) {
 				<div slot="content">
 					<div>
 						<div class="d2l-rule-card-title">${this._title}</div>
-						<d2l-discover-rule-card-match-info token=${this.token} .rule="${this.rule}" ruleIndex="${this.ruleIndex}" ></d2l-discover-rule-card-match-info>
+						<div class="d2l-rule-card-profiles">Fancy Pictures</div>
+						<div class="d2l-rule-card-match-users">Matches X users</div>
 					</div>
 				</div>
 				<d2l-dropdown-context-menu text="options" slot="actions">
