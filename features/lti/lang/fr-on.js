43b841a335945aaa7795872bef5d79fd30a855cb
JenkinsReader <resupport@desire2learn.com>
2021-08-25 14:34:25 -0400

    Translation Updates

--- features/lti/lang/fr-on.js
+++ features/lti/lang/fr-on.js
@@ -1,6 +1,8 @@
 /* eslint quotes: 0 */
 export default {
+	"error-has-occured": "An error has occurred.",
 	"external-activity-check-below": "This is an external activity. Please, check it below.",
 	"external-activity": "Activité externe",
-	"open-in-new-window": "Open in new window"
+	"open-in-new-window": "Open in new window",
+	"not-found-error": "External Activity not found."
 };
